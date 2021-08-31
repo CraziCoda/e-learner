50b173d8a8dfb0e4b1798ddf70b85cde091cd3ed
CraziCoda <64246414+CraziCoda@users.noreply.github.com>
2021-08-31 10:16:16 -1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("express-handlebars");
+const logger = require("morgan");
+
+//importing routes
+const routes = require("./routes/route");
+
+//setting global variables
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+//setting up view engines
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static("views"));
+
+//adding middlewares
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//using imported routes
+app.use("/", routes);
+
+//setting up server
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
