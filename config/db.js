c8948be1f5e63ef8d6c3da608b1153021ddc10e4
CraziCoda <64246414+CraziCoda@users.noreply.github.com>
2021-09-03 10:23:17 -1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const remoteMongo =
   "mongodb+srv://crazecode:mongoaccess014@cluster0.saa1c.mongodb.net/e-learner?retryWrites=true&w=majority";
 const localMongo = 'mongodb://localhost:27017/e-account';
 
-mongoose.connect(localMongo, (e)=>{
+mongoose.connect(remoteMongo, (e)=>{
     if(e) return console.log(`Error: ${e}`);
     console.log(`Database connected`);
 });
